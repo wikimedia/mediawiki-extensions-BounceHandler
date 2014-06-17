2b72a4da336b30acc019116e9e539c7b8e2149cd
Tony Thomas <01tonythomas@gmail.com>
2014-06-17 16:21:24 +0530

    Removed and renamed variables from the main configuration files
    Follow up after I02f4750cc37e7b4b928e4f20882382915e465be8
    
    Change-Id: Ia9ba239bbf31a137fbdfcb5d66eeefd9483b0519

--- BounceHandlerHooks.php
+++ BounceHandlerHooks.php
@@ -5,26 +5,24 @@
 class BounceHandlerHooks {
 
 	public static function onVERPAddressGenerate( $recip, $from, $headers ) {
-		global $wgEnableVERP;
-		if ( $wgEnableVERP ) {
-			$from->address = self::generateVERP( $recip->address );
-		}
+		$from->address = self::generateVERP( $recip->address );
 		return true;
 	}
+
 	/**
 	 * Generate VERP address
 	 * @param string reciepient email
 	 * @return string ReturnPath address
 	 */
 	protected static function generateVERP( $to ) {
-		global $wgVERPalgo, $wgVERPsecret, $wgServer, $wgSMTP;
+		global $wgVERPalgorithm, $wgVERPsecret, $wgServer, $wgSMTP;
 		if(  is_array( $wgSMTP ) && isset( $wgSMTP['IDHost'] ) && $wgSMTP['IDHost'] ) {
 			$email_domain = $wgSMTP['IDHost'];
 		} else {
 			$url = wfParseUrl( $wgServer );
 			$email_domain = $url['host'];
 		}
-		$verp_hash = hash_hmac( $wgVERPalgo, $to, $wgVERPsecret );
+		$verp_hash = hash_hmac( $wgVERPalgorithm, $to, $wgVERPsecret );
 		$email_prefix = 'bounces';
 		$returnPath = $email_prefix. '-' .$verp_hash. '@' .$email_domain;
 		return $returnPath;
