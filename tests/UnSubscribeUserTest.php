03e2b5085d39d6936cbe42e2a396b8287122ab4a
Tony Thomas <01tonythomas@gmail.com>
2014-08-13 19:54:37 +0530

    Added test to check that the unsubscribe function is working correctly
    Change-Id: I5acec101022035c0894841b0d1622b4a50295c77

---
+++ tests/UnSubscribeUserTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Class UnSubscribeUserTest
+ *
+ * @group Database
+ */
+class UnSubscribeUserTest extends MediaWikiTestCase {
+
+	function testUnSubscribeUser() {
+		$user = User::newFromName( 'TestUser' );
+		$user->setEmail( 'bob@example.ext' );
+		$user->addToDatabase();
+
+		$user->confirmEmail();
+		$user->saveSettings();
+
+		$this->assertTrue( $user->isEmailConfirmed() );
+
+		$uid = $user->getId();
+
+		$prefix = 'wiki';
+		$algorithm = 'md5';
+		$secretKey = 'mySecret';
+		$server = 'http://testwiki.org';
+		$smtp = array();
+		$bounceRecordPeriod = 604800;
+		$bounceRecordLimit = 3;
+
+
+		$this->setMwGlobals( array(
+			'wgVERPprefix' => $prefix,
+			'wgVERPalgorithm' => $algorithm,
+			'wgVERPsecret' => $secretKey,
+			'wgServer' => $server,
+			'wgSMTP' => $smtp,
+			'wgBounceRecordPeriod' => $bounceRecordPeriod,
+			'wgBounceRecordLimit' => $bounceRecordLimit
+			)
+		);
+
+		$this->setMwGlobals( 'wgVERPAcceptTime', 259200 );
+
+		$encodeVERP = new VerpAddressGenerator( $prefix, $algorithm, $secretKey, $server, $smtp );
+		$encodedAddress = $encodeVERP->generateVERP( $uid );
+
+		$emailHeaders['to'] = $encodedAddress;
+		$emailHeaders['subject'] = 'Delivery Failed';
+		$emailHeaders['x-failed-recipients'] = $user->getEmail();
+
+		$decodeVERPwithRegex = new ProcessBounceWithRegex();
+		$decodeVERPwithRegex->processBounceHeaders( $emailHeaders );
+		$decodeVERPwithRegex->processBounceHeaders( $emailHeaders );
+		$decodeVERPwithRegex->processBounceHeaders( $emailHeaders );
+		$decodeVERPwithRegex->processBounceHeaders( $emailHeaders );
+
+		$newUser = User::newFromId( $uid );
+		$this->assertFalse( $newUser->isEmailConfirmed() );
+
+	}
+
+}
