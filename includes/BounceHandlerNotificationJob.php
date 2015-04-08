dadbc12bf9494705dddc54a1552bbdde96cdda1a
Tina Johnson <tinajohnson.1234@gmail.com>
2015-04-08 19:33:54 +0530

    Trigger Echo notification when registered email address becomes unverified
    User is alerted using a Echo notification when his/her registered
    email address becomes unverified. The notification is linked to
    Special:ConfirmEmail page where users can confirm their email
    address again.  As of now a user finds out about the unverified email
    address only when he navigates to Special:Preferences page
    
    The patch would inject the Echo notification to the wiki that caused for
    the last email bounce activity
    
    Bug: T94436
    Change-Id: I43de1775fb2199c2d96c1bbe2cabd3fccecd7498

---
+++ includes/BounceHandlerNotificationJob.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Class BounceHandlerNotificationJob
+ *
+ * Class to notify a global user on a particular wiki on his E-mail address becoming un-subscribed
+ * @file
+ * @ingroup JobQueue
+ * @author Tony Thomas
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+class BounceHandlerNotificationJob extends Job {
+	public function __construct( Title $title, array $params ) {
+		parent::__construct( 'BounceHandlerNotificationJob', $title, $params );
+	}
+
+	public function run() {
+		$failedUserId = $this->params['failed-user-id'];
+		$failedUserEmailAddress = $this->params['failed-email'];
+		$wikiId = $this->params['wikiId'];
+		$bounceRecordPeriod = $this->params['bounceRecordPeriod'];
+		$bounceRecordLimit = $this->params['bounceRecordLimit'];
+		$bounceHandlerUnconfirmUsers = $this->params['bounceHandlerUnconfirmUsers'];
+
+		if ( $failedUserEmailAddress && $failedUserId ) {
+			$unsubscribeNotification = new BounceHandlerActions( $wikiId, $bounceRecordPeriod, $bounceRecordLimit, $bounceHandlerUnconfirmUsers );
+			$unsubscribeNotification->createEchoNotification( $failedUserId, $failedUserEmailAddress );
+		}
+
+		return true;
+
+	}
+}
