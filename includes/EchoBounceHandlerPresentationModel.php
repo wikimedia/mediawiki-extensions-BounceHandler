d8eebdb205f3f12cd1086635a23441f9e8a41886
Matthias Mullie <git@mullie.eu>
2015-11-20 16:02:02 +0100

    Notifications presentation models
    Bug: T116851
    Change-Id: Id1e28bfa96ae097b3f3024159ee9890c41934aaa

---
+++ includes/EchoBounceHandlerPresentationModel.php
@@ -0,0 +1,35 @@
+<?php
+
+class EchoBounceHandlerPresentationModel extends EchoEventPresentationModel {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getIconType() {
+		return 'placeholder';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getPrimaryLink() {
+		return array(
+			SpecialPage::getTitleFor( 'ConfirmEmail' )->getFullURL(),
+			$this->msg( 'notification-link-text-change-email' )->text()
+		);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getHeaderMessage() {
+		$msg = parent::getHeaderMessage();
+
+		// params 1 & 2 are automatically added by parent, but for this
+		// notification they'll always be null & won't be used in any message;
+		// below messages with be param 3 & 4
+		$msg->params( $this->event->getExtraParam( 'failed-email' ) );
+		$msg->params( $this->getViewingUserForGender() );
+
+		return $msg;
+	}
+}
