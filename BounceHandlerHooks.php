f760a2d9d3efa31a791e5604abd2a510cb6360e0
Reedy <reedy@wikimedia.org>
2016-10-01 11:01:37 +0100

    Remove UnitTestsList hook subscriber
    Bug: T142121
    Change-Id: Iac91c86fc66ef99bbc838b7458e8efcdcd2bcfb8

--- BounceHandlerHooks.php
+++ BounceHandlerHooks.php
@@ -63,34 +63,6 @@ class BounceHandlerHooks {
 	}
 
 	/**
-	 * Hook to add PHPUnit test cases.
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/UnitTestsList
-	 *
-	 * @param array &$files
-	 *
-	 * @return bool
-	 */
-	public static function registerUnitTests( array &$files ) {
-		// @codeCoverageIgnoreStart
-		$directoryIterator = new RecursiveDirectoryIterator( __DIR__ . '/tests/' );
-
-		/**
-		 * @var SplFileInfo $fileInfo
-		 */
-		$ourFiles = [];
-		foreach ( new RecursiveIteratorIterator( $directoryIterator ) as $fileInfo ) {
-			if ( substr( $fileInfo->getFilename(), -8 ) === 'Test.php' ) {
-				$ourFiles[] = $fileInfo->getPathname();
-			}
-		}
-
-		$files = array_merge( $files, $ourFiles );
-
-		return true;
-		// @codeCoverageIgnoreEnd
-	}
-
-	/**
 	 * Add tables to Database
 	 *
 	 * @param DatabaseUpdater $updater
