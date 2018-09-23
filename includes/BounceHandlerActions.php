05ae49659c383e2d01cd2395edf13318f6e372d0
Seb35 <seb35@seb35.fr>
2018-09-23 15:55:10 +0200

    Unsubscribe the local user if the global user is not attached
    When CentralAuth is activated, the unsubscribing action should
    be done on the local user if the global user does not exist or
    is not attached.
    
    Bug: T202455
    Change-Id: I10c25453dc349de42ea6f8c20300fe2f981d1ee6
    (cherry picked from commit a50782e526cf71c3a032dd9f337772cd14b209f1)

--- includes/BounceHandlerActions.php
+++ includes/BounceHandlerActions.php
@@ -145,9 +145,11 @@ class BounceHandlerActions {
 		$user = User::newFromId( $bounceUserId );
 		$stats = \MediaWiki\MediaWikiServices::getInstance()->getStatsdDataFactory();
 		// Handle the central account email status (if applicable)
+		$unsubscribeLocalUser = true;
 		if ( ExtensionRegistry::getInstance()->isLoaded( 'CentralAuth' ) ) {
 			$caUser = CentralAuthUser::getMasterInstance( $user );
 			if ( $caUser->isAttached() ) {
+				$unsubscribeLocalUser = false;
 				$caUser->setEmailAuthenticationTimestamp( null );
 				$caUser->saveSettings();
 				$this->notifyGlobalUser( $bounceUserId, $originalEmail );
@@ -158,7 +160,8 @@ class BounceHandlerActions {
 				);
 				$stats->increment( 'bouncehandler.unsub.global' );
 			}
-		} else {
+		}
+		if ( $unsubscribeLocalUser ) {
 			// Invalidate the email-id of a local user
 			$user->setEmailAuthenticationTimestamp( null );
 			$user->saveSettings();
